579143d92ba9684353aa2c9a6fdf3d2a66bd4aca
zehracakir <cakirzehra312@gmail.com>
2024-05-09 12:07:05 +0300

    Basic asp.net core mvc project created

---
+++ TechCareerMVCFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechCareerMVCFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
