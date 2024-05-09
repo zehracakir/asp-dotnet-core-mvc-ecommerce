34ec5d4732ef8a45f436ca1a1cdb760f9137e9dd
zehracakir <cakirzehra312@gmail.com>
2024-05-09 15:01:59 +0300

    A controller was created for the KiyafetTuru model. In accordance with the MVC structure, communication between the model and view was provided by the controller. The data in the KiyafetTurleri table was printed on the screen in a table.

---
+++ TechCareerMVCFinal/Controllers/KiyafetTuruController.cs
@@ -0,0 +1,24 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+using TechCareerMVCFinal.Data;
+using TechCareerMVCFinal.Models;
+
+namespace TechCareerMVCFinal.Controllers
+{
+    public class KiyafetTuruController : Controller
+    {
+        // dependecy injenction kullandim -> singleton design pattern
+
+        private readonly ApplicationDbContext _applicationDbContext;
+        public KiyafetTuruController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            // Index action cagrildigi zaman db ye gidecek ve KiyafetTurlerini getirecek bize bu liste ile.
+            List<KiyafetTuru> kiyafetTuruList = _applicationDbContext.KiyafetTurleri.ToList();  
+
+            return View(kiyafetTuruList);
+        }
+    }
+}
